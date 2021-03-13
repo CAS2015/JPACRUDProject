c42876d146af1badadea6a5cef3e4467b72a23e4
Your Name <you@example.com>
2021-03-12 17:30:01 -0700

    Stub Boot/MVC project, link JPA dependency

---
+++ DiverLogCrud/src/main/java/com/skilldistillery/diverlog/DiverLogCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.diverlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiverLogCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiverLogCrudApplication.class, args);
+	}
+
+}
---
+++ DiverLogCrud/src/test/java/com/skilldistillery/diverlog/DiverLogCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.diverlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiverLogCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
